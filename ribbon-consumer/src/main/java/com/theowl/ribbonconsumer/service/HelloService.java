ecabaf55544c93c87e8698ed64dac5fda7a051f0
wangran <theowl@126.com>
2018-09-18 07:19:34 +0800

    Add spend time

--- ribbon-consumer/src/main/java/com/theowl/ribbonconsumer/service/HelloService.java
+++ ribbon-consumer/src/main/java/com/theowl/ribbonconsumer/service/HelloService.java
@@ -1,6 +1,8 @@
 package com.theowl.ribbonconsumer.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,13 +10,23 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class HelloService {
 
+    Logger logger = LoggerFactory.getLogger(HelloService.class);
+
     @Autowired
     RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "helloFallback")
     public String helloService() {
 
-        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+        long start = System.currentTimeMillis();
+
+        String body = restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+
+        long end = System.currentTimeMillis();
+
+        logger.info("Spend time : " + (end - start));
+
+        return body;
     }
 
     public String helloFallback() {
